612ba56d115dcef7a189bebb1560eb853e05dede
marcgrey <49396518+marcgrey@users.noreply.github.com>
2020-11-12 15:22:26 +0800

    Initial Change

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿var carPage = $('.carpage');
+var wheelPage = $('.wheelpage');
+
+function showWheelsMenu() {
+    $(carPage).attr('class', 'carpage carhide');
+    setTimeout(()=> {
+        $(wheelPage).css('display', '');
+        $(wheelPage).attr('class', 'wheelpage wheelshow');
+    }, 1500);
+}
+
+function showMainMenu() {
+    $(wheelPage).attr('class', 'wheelpage wheelhide');
+    setTimeout(() => {
+        $(carPage).attr('class', 'carpage carshow');
+        $(wheelPage).css('display', 'none');
+    }, 600);
+}
